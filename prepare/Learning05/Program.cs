4cf93089dc2d954a1ed996fe13e7d53620830ac0
mrbourdet <mrbourdet@yahoo.com>
2023-11-06 16:43:42 -0500

    shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Blue", 3, 3);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Green", 3);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+        Console.WriteLine(s.GetColor());
+        Console.WriteLine(s.GetArea());
+        }
     }
 }
