4206a0f2ecd17b3820c045acecb695029cf755f6
mrbourdet <mrbourdet@yahoo.com>
2023-12-07 12:32:08 -0500

    minor edit

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -15,17 +15,21 @@ class Program
 
         Reception reception1 = new Reception("Reception", "Olaf & Elsa Anderson Wedding Reception", "A wedding reception for Olaf and Elsa.  Brunch will be served.", "Saturday, March 16, 2024", "10:00 a.m.", address3, "anderson031624@receptions.com");
         // New Reception
+        Console.WriteLine("===================================================================================");
         reception1.DisplayStandardMessage();
         reception1.DisplayFullReceptionMessage();
         reception1.DisplayShortMessage();
+
         Lecture lecture1 = new Lecture("Lecture", "Declining Water Levels in Eastern Idaho", "Lecture with Kassidy Telford Discussing strategies for preparing for water shortages. Hotcake breakfast served", "Saturday, December 16, 2023", "9:00 a.m.", address2, "Kassidy Telford, Director of IDWR, Bingham Ground Water District", 100);
         // New Lecture
+        Console.WriteLine("===================================================================================");
         lecture1.DisplayStandardMessage();
         lecture1.DisplayFullLectureMessage();
         lecture1.DisplayShortMessage();
         
         OutdoorGathering outdoorGathering1 = new OutdoorGathering("OutdoorGathering", "Christmas Market", "Come and see everything and anything to decorate your home for Christmas.", "Saturday, December 9, 2023", "10:00 a.m.", address1, "Weather is expected to be chilly but dry.  Dress warmly.");
         // New OutdoorGathering
+        Console.WriteLine("===================================================================================");
         outdoorGathering1.DisplayStandardMessage();
         outdoorGathering1.DisplayFullOutdoorGatheringMessage();
         outdoorGathering1.DisplayShortMessage();
